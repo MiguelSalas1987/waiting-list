6a396249b646a53dec255ae553909f4f68163ac0
Miguel Salas <miguelsalas.git@gmail.com>
2017-01-05 00:46:17 -0400

    email validation

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,11 @@
 class Request < ApplicationRecord
-  validates :name, :email, :phone_number, :paragraph, presence: true
+  validates :name, :phone_number, :paragraph, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+            #this implicitely has uniqueness true
 
   def accept!
     if confirmed?
